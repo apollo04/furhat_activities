1a482e6165ea5ac165da2dc126a51d2d514600e9
Jola Lenivchenko <42499216+JL-111@users.noreply.github.com>
2024-05-12 17:20:28 +0600

    commit all

--- server/actions/animals/animals_scripts/CatKAZ.py
+++ server/actions/animals/animals_scripts/CatKAZ.py
@@ -6,7 +6,11 @@ def run_action(ip: str):
     furhat = FurhatRemoteAPI(ip)
     furhat.set_face(mask="adult", character="Samuel")
     
-    url_list = ["http://novators.kz/audio/Animals/Animals2/AnimalsKAZ/CatKAZ/catKAZ1.wav", "http://novators.kz/audio/Animals/Animals2/AnimalsSounds/cat.wav", "http://novators.kz/audio/Animals/Animals1/AnimalsKAZ/CatKAZ/catKAZ2.wav"]
+    url_list = ["http://novators.kz/audio/Animals/Animals2/AnimalsKAZ/CatKAZ/catKAZ1.wav", "http://novators.kz/audio/Animals/Animals2/AnimalsSounds/cat.wav"]
 
     for url in url_list:
+        furhat.say(url=url, lipsync=True)
+    furhat.pauseSpeaking(500)
+    url_list2 = ["http://novators.kz/audio/Animals/Animals1/AnimalsKAZ/CatKAZ/catKAZ2.wav"]
+    for url in url_list2:
         furhat.say(url=url, lipsync=True)
