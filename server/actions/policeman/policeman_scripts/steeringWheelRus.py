fdf7929647a0f9f3e667c3f5e4e86d12851d8e5e
Jola Lenivchenko <42499216+JL-111@users.noreply.github.com>
2024-05-16 15:21:23 +0600

    commit all

--- server/actions/policeman/policeman_scripts/steeringWheelRus.py
+++ server/actions/policeman/policeman_scripts/steeringWheelRus.py
@@ -9,6 +9,7 @@ def run_action(ip: str):
     furhat.set_face(mask="adult", character="James")
     furhat.say(url='http://novators.kz/audio/PolicemanRUS/PolicemanGameRUS/Policeman16.wav', lipsync=True)
     furhat.pauseSpeaking(10)
+    furhat.resumeSpeaking()
     url_list = [
         'http://novators.kz/audio/PolicemanRUS/PolicemanGameRUS/Policeman17.wav',
         'http://novators.kz/audio/PolicemanRUS/PolicemanGameRUS/Policeman18.wav',
